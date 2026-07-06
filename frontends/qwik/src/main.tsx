35fddd9ffe66a297933e6c9f9fc28566e02286d9
Kamil Kiewisz <kamkie@outlook.com>
2026-07-06 15:47:08 +0200

    Load Qwik event runtime in production

--- frontends/qwik/src/main.tsx
+++ frontends/qwik/src/main.tsx
@@ -1,7 +1,10 @@
-import "@builder.io/qwik/qwikloader.js";
-
 import { render } from "@builder.io/qwik";
+import qwikLoaderUrl from "@builder.io/qwik/qwikloader.js?url";
 import Root from "./root";
+
+const loader = document.createElement("script");
+loader.src = qwikLoaderUrl;
+document.head.appendChild(loader);
 
 if (import.meta.env.DEV) {
   void import("./dev/forwardConsoleToDevServer").then(({ forwardConsoleToDevServer }) => {
