c1b2313d2c4aac6d3086249ca9bebb2f8fccbf4f
Kamil Kiewisz <kamkie@outlook.com>
2026-07-11 12:28:36 +0200

    chore(phoenix): update Elixir and OTP

--- backends/elixir-phoenix/mix.exs
+++ backends/elixir-phoenix/mix.exs
@@ -5,7 +5,7 @@ defmodule StackverseBackend.MixProject do
     [
       app: :stackverse_backend,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
